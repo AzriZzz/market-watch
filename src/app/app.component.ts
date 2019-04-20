a6321c91002f8f8715103896aae78996ed071328
Azri Zainal <azri.zainal@lionandlion.com>
2019-04-20 18:32:25 +0800

    remove from list working as intended

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'market-watch';
+  tempValue: any = [];
+
+  chartList = [
+    'AUDUSD',
+    'EURUSD',
+    'GBPUSD',
+    'USDCAD',
+    'USDJPY',
+    'USDCHF',
+    'AUDCAD',
+    'AUDCHF',
+    'AUDJPY',
+    'AUDNZD',
+    'CADCHF',
+    'CHFJPY',
+    'EURAUD',
+    'EURCAD',
+    'EURGBP',
+    'EURJPY',
+    'EURNZD',
+    'EURCHF',
+    'GBPAUD',
+    'GBPCAD',
+    'GBPCHF',
+    'GBPJPY',
+    'GBPNZD',
+    'NZDCAD',
+    'NZDCHF',
+    'NZDJPY',
+    'NZDUSD',
+    'XAUUSD'
+  ];
+
+  selectChart = [
+    'AUD', 'CAD', 'CHF', 'EUR', 'GBP', 'JPY', 'NZD', 'USD', 'XAU'
+  ];
+
+  onChange(value) {
+    // console.log(this.chartList, 'list');
+    for (var i in this.chartList) {
+      //check string dalam selectchart exist dalam chartlist
+      if (this.chartList[i].indexOf(value) >= 0) {
+        //tempvalue pegang value yang ada persamaan string dalam selectchart == chartlist
+        this.tempValue.push(this.chartList[i]);
+      }
+    }
+    this.chartList = this.chartList.filter(item =>
+      !this.tempValue.includes(item)
+    )
+    console.log(this.tempValue);
+    console.log(this.chartList);
+  }
+
 }
